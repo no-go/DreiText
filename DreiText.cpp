8a44ad925a35f5313474155d676d0ac67344d88e
jochen@homeland <no-go@localhost>
2017-06-30 21:55:14 +0200

    ui added to the cpp code

--- DreiText.cpp
+++ DreiText.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <fstream>
 
-#define UI_FILE "DreiText.ui"
 #define PARTSIZE 1200
 
 std::vector<char> vec;
@@ -15,6 +14,134 @@ char * textstr1 = new char[PARTSIZE];
 char * textstr2 = new char[PARTSIZE];
 char * textstr3 = new char[PARTSIZE];
 Gtk::Scale * scroller = 0;
+
+
+const gchar ui[] =
+"<interface> \
+  <requires lib='gtk+' version='3.0'/> \
+  <object class='GtkAdjustment' id='adjustment1'> \
+    <property name='upper'>1000</property> \
+    <property name='step_increment'>1</property> \
+    <property name='page_increment'>10</property> \
+  </object> \
+  <object class='GtkWindow' id='main_window'> \
+    <property name='visible'>True</property> \
+    <property name='can_focus'>False</property> \
+    <property name='title' translatable='yes'>DreiText</property> \
+    <property name='default_width'>500</property> \
+    <property name='default_height'>400</property> \
+    <child> \
+      <object class='GtkBox'> \
+        <property name='visible'>True</property> \
+        <property name='can_focus'>False</property> \
+        <property name='orientation'>vertical</property> \
+        <child> \
+          <object class='GtkBox'> \
+            <property name='visible'>True</property> \
+            <property name='can_focus'>False</property> \
+            <property name='spacing'>0</property> \
+            <property name='homogeneous'>True</property> \
+            <child> \
+              <object class='GtkScrolledWindow'> \
+                <property name='visible'>True</property> \
+                <property name='can_focus'>True</property> \
+                <child> \
+                  <object class='GtkTextView' id='text1'> \
+                    <property name='visible'>True</property> \
+                    <property name='can_focus'>True</property> \
+                    <property name='editable'>False</property> \
+                    <property name='wrap_mode'>char</property> \
+                    <property name='left_margin'>5</property> \
+                    <property name='right_margin'>5</property> \
+                    <property name='top_margin'>5</property> \
+                    <property name='bottom_margin'>5</property> \
+                  </object> \
+                </child> \
+              </object> \
+              <packing> \
+                <property name='expand'>False</property> \
+                <property name='fill'>True</property> \
+                <property name='position'>0</property> \
+              </packing> \
+            </child> \
+            <child> \
+              <object class='GtkScrolledWindow'> \
+                <property name='visible'>True</property> \
+                <property name='can_focus'>True</property> \
+                <property name='shadow_type'>in</property> \
+                <child> \
+                  <object class='GtkTextView' id='text2'> \
+                    <property name='visible'>True</property> \
+                    <property name='can_focus'>True</property> \
+                    <property name='editable'>False</property> \
+                    <property name='wrap_mode'>char</property> \
+                    <property name='left_margin'>5</property> \
+                    <property name='right_margin'>5</property> \
+                    <property name='top_margin'>5</property> \
+                    <property name='bottom_margin'>5</property> \
+                  </object> \
+                </child> \
+              </object> \
+              <packing> \
+                <property name='expand'>False</property> \
+                <property name='fill'>True</property> \
+                <property name='position'>1</property> \
+              </packing> \
+            </child> \
+            <child> \
+              <object class='GtkScrolledWindow'> \
+                <property name='visible'>True</property> \
+                <property name='can_focus'>True</property> \
+                <child> \
+                  <object class='GtkTextView' id='text3'> \
+                    <property name='visible'>True</property> \
+                    <property name='can_focus'>True</property> \
+                    <property name='editable'>False</property> \
+                    <property name='wrap_mode'>char</property> \
+                    <property name='left_margin'>5</property> \
+                    <property name='right_margin'>5</property> \
+                    <property name='top_margin'>5</property> \
+                    <property name='bottom_margin'>5</property> \
+                  </object> \
+                </child> \
+              </object> \
+              <packing> \
+                <property name='expand'>False</property> \
+                <property name='fill'>True</property> \
+                <property name='position'>2</property> \
+              </packing> \
+            </child> \
+          </object> \
+          <packing> \
+            <property name='expand'>True</property> \
+            <property name='fill'>True</property> \
+            <property name='position'>0</property> \
+          </packing> \
+        </child> \
+        <child> \
+          <object class='GtkScale' id='scroller'> \
+            <property name='visible'>True</property> \
+            <property name='can_focus'>True</property> \
+            <property name='adjustment'>adjustment1</property> \
+            <property name='restrict_to_fill_level'>False</property> \
+            <property name='fill_level'>1000</property> \
+            <property name='round_digits'>1</property> \
+            <property name='digits'>0</property> \
+            <property name='value_pos'>right</property> \
+          </object> \
+          <packing> \
+            <property name='expand'>False</property> \
+            <property name='fill'>True</property> \
+            <property name='position'>1</property> \
+          </packing> \
+        </child> \
+      </object> \
+    </child> \
+    <child type='titlebar'> \
+      <placeholder/> \
+    </child> \
+  </object> \
+</interface> ";
 
 void subs(char * retval, std::vector<char> v, unsigned int from) {
 	for (unsigned int i=0; i<PARTSIZE; i++) {
@@ -49,7 +176,7 @@ int main (int argc, char *argv[]) {
 	//Load the Glade file and instiate its widgets:
 	Glib::RefPtr<Gtk::Builder> builder;
 	try {
-		builder = Gtk::Builder::create_from_file(UI_FILE);
+		builder = Gtk::Builder::create_from_string(ui);
 	} catch (const Glib::FileError & ex) {
 		std::cerr << ex.what() << std::endl;
 		return 1;
